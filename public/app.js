11ee87602b5948c7bf25315ebc46d3a5156a60d7
tusharthexD <80884678+tusharthexD@users.noreply.github.com>
2023-12-23 18:21:59 +0530

    app.js

--- public/app.js
+++ public/app.js
@@ -22,11 +22,10 @@ console.log("hello");
 function copyLink(element){
    let link = document.getElementById("link")
 
-link.select(); 
-link.select();
+"https://thalaforareason-70lv.onrender.com/"+link.select();
 link.setSelectionRange(0, 99999);
 document.execCommand('copy')
-const whatsappUrl = `whatsapp://send?text=${encodeURIComponent(element+ " is Thala for a reason. Are you, Check this out:")}%20${encodeURIComponent(link.value)}`;
+const whatsappUrl = `whatsapp://send?text=${encodeURIComponent(element+ " is Thala for a reason. Are you, Check this out:")}%20${encodeURIComponent("https://thalaforareason-70lv.onrender.com/"+link.value)}`;
 // Open WhatsApp with the share URL
  window.location.href = whatsappUrl;
 }
